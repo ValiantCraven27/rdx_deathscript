816aa7ad1cbb29f5a694f9a0133fdfb5458f5469
Vision <77315942+ValiantCraven27@users.noreply.github.com>
2022-09-29 05:32:15 -0400

    Add files via upload

---
+++ deathscript.lua
@@ -0,0 +1,115 @@
+RDX, Death, DSC = nil,{Counter = 15, HCore = 50, SCore = 50, DCore = 50, Weakness = true, Invincible = 5000},{} -- Settings
+Citizen.CreateThread(function()	while RDX == nil do TriggerEvent('rdx:getSharedObject', function(obj) RDX = obj end) Citizen.Wait(300) end end)
+-- Weakness not implemented yet
+StoreCoords  = 0
+local PlayerIsDead = false
+local Counter =  Death.Counter
+local SetHCore = Death.SHore
+local SetSCore = Death.SCore
+local SetDCore = Death.DCore
+
+Citizen.CreateThread(function() -- Stop Auto Respawn
+Citizen.Wait(1000)
+exports.spawnmanager:setAutoSpawn(false)
+end)
+
+Citizen.CreateThread(function() -- saveCoords
+  while true do
+    --Citizen.Wait(1)
+    local player = GetPlayerPed(-1)
+    if not IsPlayerDead(player) and not IsPedSwimming(player) and not IsEntityInWater(PlayerPedId())then
+        StoreCoords  = GetEntityCoords(PlayerPedId())                                        
+    end    
+    Citizen.Wait(10000)
+  end  
+end)
+
+Citizen.CreateThread(function() -- Death Listener
+  while true do	
+    local player = GetPlayerPed(-1)
+    if IsPlayerDead(player) and GetEntityHealth(player) < 1 then       
+        DSC:CallTimer(player)                                             
+    end
+    Citizen.Wait(300)
+  end
+end)
+
+Citizen.CreateThread(function() -- Death Listener
+  while true do	
+    Citizen.Wait(1)
+    local player = GetPlayerPed(-1)
+    if IsPlayerDead(player) and GetEntityHealth(player) < 1 then 
+        DSC:DrawTxt(" Reviving in "..Counter.."",0.50, 0.95, 0.5, 0.5, true, 255, 255, 255, 255, true)                                                    
+    end   
+  end   
+end)
+
+--[[Citizen.CreateThread(function() -- Death Listener
+  while true do	
+    Citizen.Wait(1)
+    local player = GetPlayerPed(-1)
+    if not IsPlayerDead(player) then 
+      --DSC:DrawTxt("Last Stored "..StoreCoords.."",0.25, 0.95, 0.5, 0.5, true, 255, 255, 255, 255, true)                                                    
+    end   
+  end   
+end)]]
+ 
+function DSC:CallTimer(player) -- Timer
+  Counter = Death.Counter
+  TriggerEvent('rdx:playsound', "heartbeat", 0.5) 
+  while Counter > 0 do    
+    Citizen.Wait(1000)
+    Counter = Counter - 1
+    if Counter < 1 then
+       DSC:ReviveDeadPlayer(player)
+    end
+   end
+end
+
+RegisterNetEvent('rdx:playsound') -- Play Sounds
+AddEventHandler('rdx:playsound', function(sound,volume)  
+    TriggerServerEvent('InteractSound_SV:PlayOnSource','whoareyou', 0.3) 
+    Citizen.Wait(2000)  
+    TriggerServerEvent('InteractSound_SV:PlayOnSource',sound, volume) 
+end)
+
+-- Revive
+function DSC:ReviveDeadPlayer(player)
+      DoScreenFadeOut(1000)             
+      Citizen.Wait(2000)
+      NetworkResurrectLocalPlayer(StoreCoords, 100.00, true, true, false )        
+      TriggerServerEvent("rdx_skin:loadSkin")
+      TriggerServerEvent("rdx_clothing:loadClothes")
+      Citizen.InvokeNative(0xC6258F41D86676E0,player, 0, Death.HCore) --core
+      Citizen.InvokeNative(0xC6258F41D86676E0,player, 1, Death.SCore) --core  
+      --Citizen.InvokeNative(0xC6258F41D86676E0,player, 2, Death.DCore) --core
+      Citizen.Wait(3000)  
+      DoScreenFadeIn(3000)       
+      NetworkSetLocalPlayerInvincibleTime(Death.Invincible) 
+end
+
+function DSC:DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+  local str = CreateVarString(10, "LITERAL_STRING", str)
+  SetTextScale(w, h)
+  SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+  SetTextCentre(centre)
+  if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end 
+  Citizen.InvokeNative(0xADA9255D, 1);
+  DisplayText(str, x, y)
+end
+
+RegisterCommand("revive",function()
+  local player             = GetPlayerPed(-1)   
+  local playerCoords       = GetEntityCoords(player)
+    if IsPlayerDead(player) and GetEntityHealth(player) < 1 then
+        DSC:ReviveDeadPlayer(player)
+    end        
+end)
+
+RegisterCommand("die",function()
+    local player             = PlayerPedId()   
+    local playerCoords       = GetEntityCoords(player)
+      if not IsPlayerDead(player) then
+         ApplyDamageToPed(player,500,0,0,0)
+      end        
+end) 
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'rdr3'
+author 'Vision'
+version '1.0'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+server_scripts {
+}
+
+client_scripts { 
+"deathscript.lua"
+}
